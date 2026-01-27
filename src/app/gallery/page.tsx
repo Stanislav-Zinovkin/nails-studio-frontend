c1422ba2b38e85de442c50ea16d59c57b7a1abcd
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2026-01-27 19:35:24 +0200

    pages-issue

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,8 @@
+export default function GalleryPage() {
+  return (
+    <div className="pt-32 min-h-screen">
+      <h1>Contacts</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
---
+++ src/app/prices/page.tsx
@@ -0,0 +1,8 @@
+export default function PricesPage() {
+  return (
+    <div className="pt-32 min-h-screen">
+      <h1>Contacts</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
