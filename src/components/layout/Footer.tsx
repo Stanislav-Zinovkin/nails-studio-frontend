6e54d44d25d693be55462a21b78d5dc55326fcde
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2026-01-27 20:08:07 +0200

    added-instagram-link

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer(){
            <span className='font-sans text-[11px] uppercase tracking-[0.2em] text-[#10069F]/60 text-center md:text-left'>&copy; 2026 Nails Space. All rigths reserved.</span>
 
            <div className='flex gap-6 items-center'>
-              <a href='#' className='text-[#10069F]/70 hover:text-[#10069F] transition-colors duration-300 focus:outline-none'>
+              <a href='https://www.instagram.com/vitamosondz_nailspace_manicure?igsh=MWdlb2NieTF2a280Yg==' className='text-[#10069F]/70 hover:text-[#10069F] transition-colors duration-300 focus:outline-none'>
                 <svg className='w-6 h-6 stroke-current fill-current'><use href='#icon-instagram'/></svg>
               </a>
               <a href='#' className='text-[#10069F]/70 hover:text-[#10069F] transition-colors duration-300 focus:outline-none'><svg className='w-6 h-6  fill-current'><use href='#icon-facebook'/></svg></a>
