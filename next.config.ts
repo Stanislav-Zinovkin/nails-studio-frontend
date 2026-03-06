de5cfa8b51b141429c5dd82a6ff29a0e9d19a01a
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2026-03-06 19:02:43 +0200

    added-headers

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,42 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  async headers() {
+    return [
+      {
+        //all pages
+        source: '/(/*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY', //refusing iframe(guard from Clickjacking)
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',// browser can't guess type of content
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()', //no access for devices
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; connect-src 'self' https://api.telegram.org;",
+            //CSP: approved request only by self
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -55,14 +55,6 @@ export async function POST(req: NextRequest) {
         }
         
         
-        
-        
-        
-        
-        
-        
-        
-        
         return NextResponse.json({
                 success: true,
                 message: "Book approved!"
